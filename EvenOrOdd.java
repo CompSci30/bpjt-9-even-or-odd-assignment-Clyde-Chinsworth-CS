bdfed4df2251da25afc3f131eefae4059abf6a24
Alex Yang <a.yang2@share.epsb.ca>
2018-03-17 19:32:13 +0000

    Added output for if int is even

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -9,7 +9,8 @@ public class EvenOrOdd
 		Scanner kbReader = new Scanner(System.in);
 		System.out.print("Enter an integer");
 		int num = kbReader.nextInt();
-
+                if ((num % 2) == 0) //checks remainder of num / 2, if 0 , retur$
+                	System.out.println("The integer " + num + " is even.");
 	}
 }
 
