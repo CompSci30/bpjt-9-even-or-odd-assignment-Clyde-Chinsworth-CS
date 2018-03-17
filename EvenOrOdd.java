9df3aa8f749d7adb3fec976e3f2b5118d41d8fe4
Alex Yang <a.yang2@share.epsb.ca>
2018-03-17 19:21:30 +0000

    Added user input for integer

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,9 +1,14 @@
 //BPJT 9 Even or Odd Project
 //17/03/2018
+import java.io.*;
+import java.util.*;
 public class FileName
 {
 	public static void main(String args[])
 	{
+		System kbReader = new Scanner(System.in);
+		System.out.print("Enter an integer");
+		int num = kbReader.next();
 	}
 }
 
